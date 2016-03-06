064b309e156aec3347e8b7c1736b800b53ab0395
Donny Davis <donnywdavis@icloud.com>
2016-03-05 22:16:41 -0500

    Populate table cells for the cart and allow to be deleted

--- GoodAsOldPhones/CartTableViewController.swift
+++ GoodAsOldPhones/CartTableViewController.swift
@@ -10,25 +10,42 @@ import UIKit
 
 class CartTableViewController: UITableViewController {
 
+    var ordersInCart: [Order]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let product = Product()
+        product.name = "1907 Wall Set"
+        product.productImage = "phone-fullscreen1"
+        product.cellImage = "image-cell1"
+        product.price = 59.99
+        
+        let order = Order()
+        order.product = product
+        
+        ordersInCart = [order]
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return 0
+        return ordersInCart?.count ?? 0
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCellWithIdentifier("CartCell", forIndexPath: indexPath)
+        
+        // Grab the order associated wih the cell row
+        let order = ordersInCart?[indexPath.row]
+        
+        // Populate the cell row with the product details
+        if let order = order {
+            cell.textLabel?.text = order.product?.name
+            cell.detailTextLabel?.text = String(order.product?.price)
+        }
 
         return cell
     }
@@ -37,10 +54,9 @@ class CartTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            ordersInCart?.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
 
 }
