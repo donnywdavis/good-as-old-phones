643eff44b267c59d0acaae908241df26a9aeccdf
Donny Davis <donnywdavis@icloud.com>
2016-03-05 21:29:38 -0500

    Create Orders and Order classes

---
+++ GoodAsOldPhones/Order.swift
@@ -0,0 +1,13 @@
+//
+//  Order.swift
+//  GoodAsOldPhones
+//
+//  Created by Donny Davis on 3/5/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+import UIKit
+
+class Order: NSObject {
+    var product: [Product]?
+}
---
+++ GoodAsOldPhones/Orders.swift
@@ -0,0 +1,13 @@
+//
+//  Orders.swift
+//  GoodAsOldPhones
+//
+//  Created by Donny Davis on 3/5/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+import UIKit
+
+class Orders: NSObject {
+    var orders: [Order]?
+}
